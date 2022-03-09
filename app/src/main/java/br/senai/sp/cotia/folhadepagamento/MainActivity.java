3cb90677ab1affe924b0921e6c0747b57feef83b
Maick <maickbsilva@gmail.com>
2022-03-08 21:49:15 -0300

    Versão Final App Folha de Pagamento

--- app/src/main/java/br/senai/sp/cotia/folhadepagamento/MainActivity.java
+++ app/src/main/java/br/senai/sp/cotia/folhadepagamento/MainActivity.java
@@ -19,9 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton tipoStandard, tipoBasico, tipoSuper, tipoMaster;
     private CheckBox recebeTransp, recebeAli, recebeRef;
     private Button botao;
-
-    //private radiogroup radiogroup;
-    //private checkbox checkbox;
+    private double sLiquido, totalDescontos, baseCalc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,162 +36,165 @@ public class MainActivity extends AppCompatActivity {
         recebeAli = findViewById(R.id.valeAli);
         recebeRef = findViewById(R.id.valeRef);
 
-        //radiogroup = findview (r.id.radiogroup)
+    }
 
-        //pega o valor selecionado no radiogroup
-        //radioGroup.getCheckedRadioButtonId();
+    public void irparasegunda(View view) {
 
-        //checkbox.ischecked
-
-        }
-    public void irparasegunda (View view){
-
-        if (salario.getText().toString().isEmpty()){
+        if (salario.getText().toString().isEmpty()) {
             salario.setError(getString(R.string.valida_salario)); //obrigando a preencher
             Toast.makeText(getBaseContext(), R.string.valida_salario, Toast.LENGTH_SHORT).show(); // mensagem de validacao
-        }else if (dependentes.getText().toString().isEmpty()){
+        } else if (dependentes.getText().toString().isEmpty()) {
             dependentes.setError(getString(R.string.valida_dependentes)); //obrigando a preencher
             Toast.makeText(getBaseContext(), R.string.valida_dependentes, Toast.LENGTH_SHORT).show();
-        }else {
-            Intent intecao = new Intent(this,Resultado.class);
+        } else {
 
-            //intent.putExtra("imc", imc);
-           // Intent intent = new Intent(this, ResultadoActivity.class);
-          //  intent.putExtra("imc", imc);
-           // intent.putExtra("genero",genero);
-          //  startActivity(intent);
+            Intent intecao = new Intent(this, Resultado.class);
+
+            double valorSalario = Double.parseDouble(salario.getText().toString());
+
+            double depen = Double.parseDouble(dependentes.getText().toString());
+
+            //SB –INSS -(189,59  *  número  de  dependentes).
+            baseCalc = valorSalario - inss() - 189.59 * depen;
+
+            sLiquido = valorSalario - descontoPlano() - valeTransporte() - valeAlimentacao() - valeRef() - impRenda() - inss();
+
+            totalDescontos = (descontoPlano() + valeTransporte() + valeAlimentacao() + valeRef() + impRenda() + inss()) * 100 / valorSalario;
+
+            intecao.putExtra("salarioLiquido", sLiquido);
+            intecao.putExtra("salarioBruto", valorSalario);
+            intecao.putExtra("descontoPlanoSaude", descontoPlano());
+            intecao.putExtra("descontoTransporte", valeTransporte());
+            intecao.putExtra("descontoAlimentacao", valeAlimentacao());
+            intecao.putExtra("descontoRefeicao", valeRef());
+            intecao.putExtra("descontoImpostoRenda", impRenda());
+            intecao.putExtra("descontoInss", inss());
+            intecao.putExtra("totalDescontos", totalDescontos);
             //finish();
 
             startActivity(intecao);
-
-
         }
 
     }
 
 
-    public double descontoPlano(){
+    public double descontoPlano() {
 
-            double descontoPlano;
-            double valorSalario = Double.parseDouble(salario.getText().toString());
-            double valorDependentes = Double.parseDouble(dependentes.getText().toString());
+        double descontoPlano;
+        double valorSalario = Double.parseDouble(salario.getText().toString());
+        double valorDependentes = Double.parseDouble(dependentes.getText().toString());
 
+        if (valorSalario <= 3000) {
+            if (tipoStandard.isChecked()) {
+                descontoPlano = 60;
 
-            if (valorSalario <= 3000){
-                if (tipoStandard.isChecked()){
-                    descontoPlano = 60;
+            } else if (tipoBasico.isChecked()) {
+                descontoPlano = 80;
 
-                }else if (tipoBasico.isChecked()){
-                    descontoPlano = 80;
+            } else if (tipoSuper.isChecked()) {
+                descontoPlano = 95;
 
-                }else if (tipoSuper.isChecked()){
-                    descontoPlano = 95;
-
-                }else {
-                    descontoPlano = 130;
-                }
-
-            }else {
-                if (tipoStandard.isChecked()){
-                    descontoPlano = 80;
-
-                }else if (tipoBasico.isChecked()){
-                    descontoPlano = 110;
-
-                }else if (tipoSuper.isChecked()){
-                    descontoPlano = 135;
-
-                }else {
-                    descontoPlano = 180;
-                }
-
+            } else {
+                descontoPlano = 130;
             }
+
+        } else {
+            if (tipoStandard.isChecked()) {
+                descontoPlano = 80;
+
+            } else if (tipoBasico.isChecked()) {
+                descontoPlano = 110;
+
+            } else if (tipoSuper.isChecked()) {
+                descontoPlano = 135;
+
+            } else {
+                descontoPlano = 180;
+            }
+        }
         return descontoPlano;
 
-        }
+    }
 
-        public double valeTransporte(){
+    public double valeTransporte() {
 
         double valorSalario = Double.parseDouble(salario.getText().toString());
 
-        if (recebeTransp.isChecked()){
+        if (recebeTransp.isChecked()) {
 
             return valorSalario * 0.06;
-
         }
         return 0;
-        }
+    }
 
-    public double valeAlimentacao(){
+    public double valeAlimentacao() {
 
         double valorSalario = Double.parseDouble(salario.getText().toString());
 
-        if (recebeAli.isChecked()){
+        if (recebeAli.isChecked()) {
 
-            if (valorSalario <= 3000){
+            if (valorSalario <= 3000) {
                 return 15;
-            }else if (valorSalario <= 5000){
+            } else if (valorSalario <= 5000) {
                 return 25;
-            }else {
+            } else {
                 return 35;
             }
-        }else {
+        } else {
             return 0;
         }
     }
 
-    public double valeRef(){
+    public double valeRef() {
 
         double valorSalario = Double.parseDouble(salario.getText().toString());
 
-        if (recebeRef.isChecked()){
+        if (recebeRef.isChecked()) {
 
-            if (valorSalario <= 3000){
-                return 2.60*22;
-            }else if (valorSalario <= 5000){
-                return 3.65*22;
-            }else {
-                return 6.50*22;
+            if (valorSalario <= 3000) {
+                return 2.60 * 22;
+            } else if (valorSalario <= 5000) {
+                return 3.65 * 22;
+            } else {
+                return 6.50 * 22;
             }
-        }else {
+        } else {
             return 0;
         }
     }
 
-    public double impRenda(){
-        double valorSalario = Double.parseDouble(salario.getText().toString());
+    public double impRenda() {
 
-        if (valorSalario <= 1903.98){
+        if (baseCalc <= 1903.98) {
             return 0;
-        }else if (valorSalario < 2826.65){
-            return (valorSalario * 0.075) - 142.80;
-        }else if (valorSalario < 3751.05){
-            return (valorSalario * 0.15) - 354.80;
-        }else if (valorSalario < 4664.68){
-            return (valorSalario * 0.225) - 636.13;
-        }else {
-            return (valorSalario * 0.275) - 869.36;
+        } else if (baseCalc < 2826.65) {
+            return (baseCalc * 0.075) - 142.80;
+        } else if (baseCalc < 3751.05) {
+            return (baseCalc * 0.15) - 354.80;
+        } else if (baseCalc < 4664.68) {
+            return (baseCalc * 0.225) - 636.13;
+        } else {
+            return (baseCalc * 0.275) - 869.36;
         }
 
     }
 
-    public  double inss(){
+    public double inss() {
         double valorSalario = Double.parseDouble(salario.getText().toString());
 
-        if (valorSalario <=1212){
+        if (valorSalario <= 1212) {
             return (valorSalario * 0.075);
-        }else if (valorSalario < 2427.35){
+        } else if (valorSalario < 2427.35) {
             return (valorSalario * 0.09) - 18.18;
-        }else if (valorSalario < 3641.03){
-            return  (valorSalario * 0.12) - 91;
-        }else if (valorSalario < 7087.22){
-            return  (valorSalario * 0.14) - 163.82;
-        }else {
+        } else if (valorSalario < 3641.03) {
+            return (valorSalario * 0.12) - 91;
+        } else if (valorSalario < 7087.22) {
+            return (valorSalario * 0.14) - 163.82;
+        } else {
             return 828.39;
         }
 
     }
 
 
-
-    }
+}
--- app/src/main/java/br/senai/sp/cotia/folhadepagamento/Resultado.java
+++ app/src/main/java/br/senai/sp/cotia/folhadepagamento/Resultado.java
@@ -4,27 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Resultado extends AppCompatActivity {
 
-    //declarar as variaveis
-    private double salarioBruto, salarioLiquido, vt, va, vr, inss, impRend, descPlano;
+    private double salarioBruto, salarioLiquido, vt, va, vr, inss, impRend, descontoPlanoSaude, totalDesc;
+    TextView txSalarioLiquido;
+    TextView txSalarioBruto;
+    TextView txDescontoPlano;
+    TextView txDescontoTransporte;
+    TextView txDescontoAlimentacao;
+    TextView txDescontoRefeicao;
+    TextView txDescontoImpRenda;
+    TextView txDescontoInss;
+    TextView txTotalDescontos;
 
-
-    //SB –INSS -(189,59  *  número  de  dependentes).
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
 
-
+        txSalarioLiquido = findViewById(R.id.salarioLiquido);
+        txSalarioBruto = findViewById(R.id.salarioBruto);
+        txDescontoPlano = findViewById(R.id.planoSaude);
+        txDescontoTransporte = findViewById(R.id.transporte);
+        txDescontoAlimentacao = findViewById(R.id.alimentacao);
+        txDescontoRefeicao = findViewById(R.id.refeicao);
+        txDescontoImpRenda = findViewById(R.id.impostoRenda);
+        txDescontoInss = findViewById(R.id.inss);
+        txTotalDescontos = findViewById(R.id.totalDescontos);
 
         //pegando os valores passados via 'intecao'
 
-        salarioBruto = getIntent().getDoubleExtra("salario",0);
-        vt = getIntent().getDoubleExtra("vt", 0);
-      //  imc = getIntent().getDoubleExtra("imc", 0);
-       // genero = getIntent().getStringExtra("genero");
+        salarioLiquido = getIntent().getDoubleExtra("salarioLiquido", 0);
+        salarioBruto = getIntent().getDoubleExtra("salarioBruto", 0);
+        descontoPlanoSaude = getIntent().getDoubleExtra("descontoPlanoSaude", 0);
+        vt = getIntent().getDoubleExtra("descontoTransporte", 0);
+        va = getIntent().getDoubleExtra("descontoAlimentacao", 0);
+        vr = getIntent().getDoubleExtra("descontoRefeicao", 0);
+        impRend = getIntent().getDoubleExtra("descontoImpostoRenda", 0);
+        inss = getIntent().getDoubleExtra("descontoInss", 0);
+        totalDesc = getIntent().getDoubleExtra("totalDescontos", 0);
+
+        txSalarioLiquido.setText("R$ " + String.format("%.2f", salarioLiquido));
+        txSalarioBruto.setText("R$ " + String.format("%.2f", salarioBruto));
+        txDescontoPlano.setText("R$ " + String.format("%.2f", descontoPlanoSaude));
+        txDescontoTransporte.setText("R$ " + String.format("%.2f", vt));
+        txDescontoAlimentacao.setText("R$ " + String.format("%.2f", va));
+        txDescontoRefeicao.setText("R$ " + String.format("%.2f", vr));
+        txDescontoImpRenda.setText("R$ " + String.format("%.2f", impRend));
+        txDescontoInss.setText("R$ " + String.format("%.2f", inss));
+        txTotalDescontos.setText(String.format("%.2f", totalDesc) + "%");
+
     }
 }
